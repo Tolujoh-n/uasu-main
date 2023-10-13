7062e7d05b5e8b1407c1068c02b065a5203ddd1c
Tolujoh-n <tolujohnofficial@gmail.com>
2023-10-13 12:20:24 +0100

    more responsiveness added

--- src/Components/Waitlist.js
+++ src/Components/Waitlist.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ReCAPTCHA from "react-google-recaptcha";
 import logo from "../assets/img/uasu-logo.png";
