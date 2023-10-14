6fe2b98c5920c068250543d7805c6630c7b9cee8
Rapha-btc <rapha@fontainebleau-management.com>
2023-10-14 20:02:59 +0000

    rafa and tolu work

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
             <h1>Freeze your Bitcoin with full Collateralization</h1>
           </div>
           <div className="col-xl-4 col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="150">
-            <img src={featureImage} className="img-fluid animated" alt="" />
+            <img src={featureImage} id="heading" className="img-fluid animated" alt="" />
           </div>
           <div className="col-xl-5 col-lg-6 pt-3 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
             <h2>Unstoppable payoff on Bitcoin</h2>
